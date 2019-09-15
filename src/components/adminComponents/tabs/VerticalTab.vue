6d12fbc95adf7a4e2d4b11d011825b2539eeb61d
Jama Mohamed <jama3137@gmail.com>
2019-09-15 14:52:00 +0300

    Added the admin vertical tab

---
+++ src/components/adminComponents/tabs/VerticalTab.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-tabs vertical dark background-color="primary">
+    <v-tab>Hospitals</v-tab>
+    <v-tab>Donors</v-tab>
+    <v-tab-item>
+      Hospitals
+    </v-tab-item>
+    <v-tab-item>
+      Donors
+    </v-tab-item>
+  </v-tabs>
+</template>
+
+<script>
+
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+.v-tabs {
+  height: inherit;
+}
+</style>
--- src/views/adminViews/Home.vue
+++ src/views/adminViews/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home-container">
     <Appbar />
+    <VerticalTab />
   </div>
 </template>
 
 <script>
 import Appbar from "../../components/adminComponents/appbar/Appbar"
+import VerticalTab from "../../components/adminComponents/tabs/VerticalTab";
 export default {
   components: {
-    Appbar
+    Appbar,
+    VerticalTab,
   }
 };
 </script>
